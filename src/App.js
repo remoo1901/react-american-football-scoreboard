9625d928b67f3744772b58bd7c5c26c35403e588
remoo1901 <remoo1901@hotmail.com>
2019-12-18 23:02:56 -0500

    MVP Completed

--- src/App.js
+++ src/App.js
@@ -20,12 +20,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score"> { lionsCount + 32}</div>
+            <div className="home__score"> { lionsCount}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{ tigersCount + 32}</div>  
+            <div className="away__score">{ tigersCount}</div>  
           </div>
         </div>
         <BottomRow />
