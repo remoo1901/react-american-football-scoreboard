9c6ba14cd4d3aeef84a8137ece87caa00302d4ca
remoo1901 <remoo1901@hotmail.com>
2019-12-18 21:49:44 -0500

    MVP completed

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
- const [lionsCount, setCount] = useState(0);
- const [tigerCount, setCount] = useState(0);
+ const [lionsCount, lionsSetCount] = useState(0);
+ const [tigersCount, tigersSetCount] = useState(0);
 
- 
+
 
   return (
     <div className="container">
@@ -25,7 +25,7 @@ function App() {
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{ tigerCount + 32}</div>  
+            <div className="away__score">{ tigersCount + 32}</div>  
           </div>
         </div>
         <BottomRow />
@@ -33,12 +33,14 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick = {() =>{ lionsSetCount(lionsCount + 7)}} >Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick = {() => { lionsSetCount(lionsCount + 3)}} >Home Field Goal</button>
+          
+            
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick = {()=> { tigersSetCount(tigersCount + 7)}} >Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick = {()=> { tigersSetCount(tigersCount + 3)}} >Away Field Goal</button>
         </div>
       </section>
     </div>
@@ -46,3 +48,30 @@ function App() {
 }
 
 export default App;
+
+
+
+
+ /*
+ const lionsTD = () => {
+  lionsSetCount(lionsCount + 6);
+ }
+
+ const lionsFG = () => {
+  lionsSetCount(lionsCount + 3);
+}
+
+
+ const tigerTD = () => {
+  tigersSetCount(tigersCount + 6);
+ }
+
+ const tigerFG = () => {
+  tigersSetCount(tigersCount + 3);
+ }
+
+  const reset = () => {
+   lionsSetCount(0)
+   tigersSetCount(0)
+
+ }  */
